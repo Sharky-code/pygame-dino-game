bd1f243d5395efc1fceb81f7db09a3777b4844eb
Sharky-code <ethanpunhk@gmail.com>
2022-06-22 20:24:25 +0100

    added the flying obstacles (not finished)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Add screen stretching later
-import pygame
+import pygame, random, copy
 width, height = 400, 200
 frameRate = 60
 
@@ -15,14 +15,23 @@ image = pygame.image.load(r"player.png")
 
 class gameLogic:
     """Controls the player object, including the gravity"""
+
     def __init__(self) -> None:
         self.objectDimension = [10, 25] #object dimension -> width, height 
         self.objectVelocity = [0, 0]
         self.objectLocation = [width / 4 - self.objectDimension[0], height / 2 - self.objectDimension[1]]
+
         self.frameRate = frameRate
-        self.gravity = -10
+        self.gravity = -15
         self.groundLevel = [0, 200] #the empty value is reverved for later :)
         self.onGround = False
+
+        self.enemyList = {'dino' : [], 'bird' : []}
+        self.previousTimeEnemy = 0
+        #The first item was minium time
+        self.randomRangeEnemy = [0.3, 2]
+        self.randomTimeFrameEnemy = random.uniform(self.randomRangeEnemy[0], self.randomRangeEnemy[1])
+        self.speed = 2.5
 
     def playerGravity(self) -> None:
         """
@@ -47,8 +56,41 @@ class gameLogic:
             self.onGround = True
 
     def drawBackground(self) -> None:
+        """I'll draw the clouds and sky later"""
         #draw the floor
         pygame.draw.rect(screen, (100, 100, 100), pygame.Rect(0, 100, width, height))
+
+    def drawEnemy(self) -> None:
+        """
+        I need the enemy to be distant apart.
+
+        I will use time to determine how it will spawn, because the faster
+        the object goes the harder you jump.
+
+        The speed increase are also constant for all objects, as the effect
+        isn't that the enemy is going faster but you are running faster.
+
+        I'm also gonna make the birds later. It shouldn't be the hardest to
+        make but I'm going to make stuff one by one.
+        """
+
+        if (pygame.time.get_ticks() - self.previousTimeEnemy) > self.randomTimeFrameEnemy * 1000:
+            self.enemyList[list(self.enemyList.keys())[random.randrange(0, 1)]].append(width)
+            self.previousTimeEnemy = copy.deepcopy(pygame.time.get_ticks())
+            self.randomTimeFrameEnemy = random.uniform(self.randomRangeEnemy[0], self.randomRangeEnemy[1])
+
+        self.speed += 0.0001 #put this to the if loop above if you want
+
+        print(self.enemyList)
+        for y in list(self.enemyList.keys()):
+            for x in range(len(self.enemyList[y])):
+                try: 
+                    pygame.draw.rect(screen, (10, 10, 10), pygame.Rect(self.enemyList[y][x], 100 - 10, 10, 10)) 
+                    self.enemyList[y][x] -= self.speed
+
+                    if self.enemyList[y][x] < -50:
+                        self.enemyList[y].pop(x)
+                except: pass 
 
 
 object = gameLogic()
@@ -60,19 +102,25 @@ while not endGame:
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT: endGame = True #makes you able to close the script (don't remove this)
     
-    if pygame.key.get_pressed()[pygame.K_UP] and object.onGround:
+    if (pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_SPACE]) and object.onGround:
         object.objectVelocity = [0,200]
 
     screen.fill((255,255,255)) #fill the screen with the RGB color
     #hitbox visualizer:
     #pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(object.objectLocation[0], height - object.objectLocation[1] + 100, object.objectDimension[0], object.objectDimension[1]))
 
-    #stuff i need to do later on: make the image stretch according to the different dimension i set
-    screen.blit(image, [object.objectLocation[0], height - object.objectLocation[1] + 100])
-
     object.drawBackground()
     object.playerGravity()
-    print(object.objectVelocity, object.objectLocation, height - object.groundLevel[1])
+    object.drawEnemy()
+    print(object.objectVelocity, object.objectLocation, height - object.groundLevel[1], pygame.time.get_ticks())
+    #stuff i need to do later on: make the image stretch according to the different dimension i set
+    #self.enemyList = {'dino' : [], 'bird' : []}
+    screen.blit(image, [object.objectLocation[0], height - object.objectLocation[1] + 100])
+
+    for y in list(object.enemyList.keys()):
+        for x in object.enemyList[y]:
+            #just for testing I'll add the variable for it after. It also doesn't change if you change the background
+            pygame.draw.rect(screen, (10, 10, 10), pygame.Rect(x, 100 - 10, 10, 10)) 
 
     pygame.time.Clock().tick(frameRate) #should cap the fps
     pygame.display.update()
